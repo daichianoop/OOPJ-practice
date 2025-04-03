1e5164b4f52881fd61953b5ca956a1bd5ee4d016
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-04-03 12:06:58 +0530

    Delete lab_1 directory

--- lab_1/question5.java
+++
@@ -1,8 +0,0 @@
-package lab_1;
-public class question5 {
-    public static void main(String[] args) {
-        String firstName = "Anoop";
-        String lastName = "Kumar";
-        System.out.println(lastName + " " + firstName);
-    }
-}
